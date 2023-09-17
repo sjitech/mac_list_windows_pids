c9ade4457e05b5bcc2733d821fc3ef7cd1200f7a
Jerry Lee <oldratlee@gmail.com>
2023-09-17 12:52:58 +0800

    upgrade `lswin.py` to `python3` 🆕 🐍

--- lswin.py
+++ lswin.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import Quartz
 
@@ -9,22 +9,22 @@ wl = sorted(wl, key=lambda k: k.valueForKey_('kCGWindowOwnerPID'))
 
 #print wl
 
-print 'PID'.rjust(7) + ' ' + 'WinID'.rjust(5) + '  ' + 'x,y,w,h'.ljust(21) + ' ' + '\t[Title] SubTitle'
-print '-'.rjust(7,'-') + ' ' + '-'.rjust(5,'-') + '  ' + '-'.ljust(21,'-') + ' ' + '\t-------------------------------------------'
+print('PID'.rjust(7) + ' ' + 'WinID'.rjust(5) + '  ' + 'x,y,w,h'.ljust(21) + ' ' + '\t[Title] SubTitle')
+print('-'.rjust(7,'-') + ' ' + '-'.rjust(5,'-') + '  ' + '-'.ljust(21,'-') + ' ' + '\t-------------------------------------------')
 
 for v in wl:
-	print ( \
-		str(v.valueForKey_('kCGWindowOwnerPID') or '?').rjust(7) + \
-		' ' + str(v.valueForKey_('kCGWindowNumber') or '?').rjust(5) + \
-		' {' + ('' if v.valueForKey_('kCGWindowBounds') is None else \
-		 	( \
-			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('X')))     + ',' + \
-			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Y')))     + ',' + \
-			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Width'))) + ',' + \
-			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Height'))) \
-		 	) \
-		 	).ljust(21) + \
-		'}' + \
-		'\t[' + ((v.valueForKey_('kCGWindowOwnerName') or '') + ']') + \
-		('' if v.valueForKey_('kCGWindowName') is None else (' ' + v.valueForKey_('kCGWindowName') or '')) \
-	).encode('utf8')
+	print (
+		str(v.valueForKey_('kCGWindowOwnerPID') or '?').rjust(7) +
+		' ' + str(v.valueForKey_('kCGWindowNumber') or '?').rjust(5) +
+		' {' + ('' if v.valueForKey_('kCGWindowBounds') is None else
+		 	(
+			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('X')))     + ',' +
+			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Y')))     + ',' +
+			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Width'))) + ',' +
+			 	str(int(v.valueForKey_('kCGWindowBounds').valueForKey_('Height')))
+		 	)
+		 	).ljust(21) +
+		'}' +
+		'\t[' + ((v.valueForKey_('kCGWindowOwnerName') or '') + ']') +
+		('' if v.valueForKey_('kCGWindowName') is None else (' ' + v.valueForKey_('kCGWindowName') or ''))
+	)
