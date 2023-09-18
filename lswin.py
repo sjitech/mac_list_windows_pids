dc4ddd57e4d344f10f50c2034d9236b1d2830180
Jerry Lee <oldratlee@gmail.com>
2023-09-18 17:48:42 +0800

    refactor: add dataclass `WindowInfo`; extract function `list_window_infos`, `print_window_infos`

--- lswin.py
+++ lswin.py
@@ -1,34 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from dataclasses import dataclass
+from typing import Tuple, Iterable
+
 import Quartz
 
-# win_list = Quartz.CGWindowListCopyWindowInfo( Quartz.kCGWindowListOptionOnScreenOnly | Quartz.kCGWindowListExcludeDesktopElements, Quartz.kCGNullWindowID)
-win_list = Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionAll, Quartz.kCGNullWindowID)
-win_list = sorted(win_list, key=lambda k: k.valueForKey_('kCGWindowOwnerPID'))
 
-# print head
-print('    PID  WinID  (x, y, w, h)              [Title] SubTitle')
-print('-------  -----  ------------------------  -------------------------------------------')
+@dataclass
+class WindowInfo:
+    pid: int
+    win_id: int
 
-# print items
-for w in win_list:
-    win_rect = (
-        f"("
-        f"{int(w.valueForKey_('kCGWindowBounds').valueForKey_('X'))},"
-        f" {int(w.valueForKey_('kCGWindowBounds').valueForKey_('Y'))},"
-        f" {int(w.valueForKey_('kCGWindowBounds').valueForKey_('Width'))},"
-        f" {int(w.valueForKey_('kCGWindowBounds').valueForKey_('Height'))}"
-        f")"
+    rect: Tuple[int, int, int, int]
+    """
+    rect: (x, y, width, height)
+    """
+
+    title: str
+    subtitle: str
+
+
+def list_window_infos() -> Iterable[WindowInfo]:
+    return (
+        WindowInfo(
+            pid=int(w.valueForKey_('kCGWindowOwnerPID')),
+            win_id=int(w.valueForKey_('kCGWindowNumber')),
+            rect=(
+                int(w.valueForKey_('kCGWindowBounds').valueForKey_('X')),
+                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Y')),
+                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Width')),
+                int(w.valueForKey_('kCGWindowBounds').valueForKey_('Height')),
+            ),
+            title=str(w.valueForKey_('kCGWindowOwnerName') or ''),
+            subtitle=str(w.valueForKey_('kCGWindowName') or '')
+        )
+        for w in Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListOptionAll, Quartz.kCGNullWindowID)
     )
-    subtitle = w.valueForKey_('kCGWindowName')
-    title_info = (
-        f"[{w.valueForKey_('kCGWindowOwnerName') or ''}]"
-        f"{'' if not subtitle else ' ' + subtitle}"
-    )
 
-    print(
-        f"{w.valueForKey_('kCGWindowOwnerPID') or '?': >7}"
-        f"  {w.valueForKey_('kCGWindowNumber') or '?': >5}"
-        f"  {win_rect: <24}  {title_info}"
+
+def print_window_infos(win_list: Iterable[WindowInfo]):
+    # print head
+    print('    PID  WinID  (x, y, w, h)              [Title] SubTitle')
+    print('-------  -----  ------------------------  --------------------------------------')
+    # print items
+    for w in win_list:
+        title_info = f"[{w.title or ''}]{'' if not w.subtitle else ' ' + w.subtitle}"
+        print(f"{w.pid: >7}  {w.win_id: >5}  {w.rect!r: <24}  {title_info}")
+
+
+if __name__ == '__main__':
+    print_window_infos(sorted(
+        list_window_infos(),
+        key=lambda k: (k.pid, k.win_id))
     )
