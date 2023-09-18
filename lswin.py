8f0ec10b1a58476cfb9856471dec87486e08dc6e
Jerry Lee <oldratlee@gmail.com>
2023-09-18 20:51:40 +0800

    feat: add command option `-k/--sort-key`

--- lswin.py
+++ lswin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from dataclasses import dataclass
+import sys
+from dataclasses import dataclass, fields
 from itertools import tee
 from optparse import OptionParser
 from typing import Tuple, Iterable
@@ -67,23 +68,56 @@ def print_window_infos(win_list: Iterable[WindowInfo]):
 
 
 if __name__ == '__main__':
+    rect_index = {'x': 0, 'y': 1, 'width': 2, 'height': 3}
+    supported_sort_keys = tuple(
+        [f.name for f in fields(WindowInfo) if f.name != 'rect']
+        + list(rect_index.keys())
+    )
+    sort_keys_str = ', '.join(supported_sort_keys)
+
+    ############################################################
+    # parse options
+    ############################################################
+
     option_parser = OptionParser(
         '%prog [OPTION]...'
         '\nlist all windows title and their owner process ids.'
         '\n\nExamples:'
         '\n  %prog'
         '\n  %prog --exclude-0-area'
+        '\n  %prog --sort-key x --sort-key title'
     )
     option_parser.add_option(
         '--exclude-0-area', dest='exclude_0_area', default=False,
         action='store_true', help='exclude windows with 0 area')
+    option_parser.add_option(
+        '-k', '--sort-key', dest='sort_key', default=[], action='append',
+        help=f"sort key, can be {sort_keys_str}; default sort key is (pid, win_id)")
     options, _ = option_parser.parse_args()
+
+    illegal_sort_keys = tuple(filter(lambda k: k not in supported_sort_keys, options.sort_key))
+    if illegal_sort_keys:
+        print(f"Unsupported sort key: {', '.join(illegal_sort_keys)}!"
+              f" supported sort keys: {sort_keys_str}", file=sys.stderr)
+        exit(1)
+
+    ############################################################
+    # biz logic
+    ############################################################
 
     windows = list_window_infos()
     if options.exclude_0_area:
         windows = filter(lambda w: w.rect[2] > 0 and w.rect[3] > 0, windows)
 
-    print_window_infos(sorted(
-        windows,
-        key=lambda w: (w.pid, w.win_id)
-    ))
+
+    def sort_key(w: WindowInfo):
+        key = []
+        for k in options.sort_key:
+            if hasattr(w, k):
+                key.append(getattr(w, k))
+            else:
+                key.append(w.rect[rect_index[k]])
+        return key + [w.pid, w.win_id]
+
+
+    print_window_infos(sorted(windows, key=sort_key))
