ce783b36624608d226e70c18968bdce220e0f30a
Jerry Lee <oldratlee@gmail.com>
2023-09-19 18:37:12 +0800

    feat: add short option `-Z`; add `metavar` for `-k` option 🎚️

--- lswin.py
+++ lswin.py
@@ -98,14 +98,14 @@ if __name__ == '__main__':
         '\n  %prog --sort-key x --sort-key title'
     )
     option_parser.add_option(
-        '--exclude-0-area', dest='exclude_0_area', default=False,
+        '-Z', '--exclude-0-area', dest='exclude_0_area', default=False,
         action='store_true', help='exclude windows with 0 area')
     option_parser.add_option(
-        '-k', '--sort-key', dest='sort_key', default=[], action='append',
-        help=f"sort key, can be {sort_keys_str}; default sort key is (pid, win_id)")
+        '-k', '--sort-key', dest='sort_keys', default=[], action='append',
+        metavar='SORT_KEY', help=f"sort key, can be {sort_keys_str}; default sort key is (pid, win_id)")
     options, _ = option_parser.parse_args()
 
-    illegal_sort_keys = tuple(filter(lambda k: k not in supported_sort_keys, options.sort_key))
+    illegal_sort_keys = tuple(filter(lambda k: k not in supported_sort_keys, options.sort_keys))
     if illegal_sort_keys:
         print(f"Unsupported sort key: {', '.join(illegal_sort_keys)}!"
               f" supported sort keys: {sort_keys_str}", file=sys.stderr)
@@ -122,6 +122,6 @@ if __name__ == '__main__':
     print_window_infos(sorted(
         windows, key=lambda w: tuple(
             getattr(w, k) if hasattr(w, k) else getattr(w.rect, k)
-            for k in options.sort_key
+            for k in options.sort_keys
         ) + (w.pid, w.win_id)
     ))
